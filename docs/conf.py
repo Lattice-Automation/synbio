aef645bd7627540561ab6f7034da6b65f82a82f7
JJTimmons <joshua.timmons1@gmail.com>
2019-08-19 11:45:04 -0400

    Bump version: 0.4.3 → 0.4.4

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2019, jjtimmons"
 author = "jjtimmons"
 
 # The full version, including alpha/beta/rc tags
-release = "0.4.3"
+release = "0.4.4"
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="synbio",
-    version="0.4.3",
+    version="0.4.4",
     author="JJTimmons",
     author_email="jtimmons@latticeautomation.com",
     description="Synbio design and build library",
