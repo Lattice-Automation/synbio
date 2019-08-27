75a19066dc43850e14ae9841631b18de97f1749e
JJTimmons <joshua.timmons1@gmail.com>
2019-08-27 12:17:23 -0400

    Bump version: 0.4.8 → 0.4.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="synbio",
-    version="0.4.8",
+    version="0.4.9",
     author="JJTimmons",
     author_email="jtimmons@latticeautomation.com",
     description="Synbio design and build library",
