af46c4730591eb328ed53dcc66db83ffad637b3f
JJTimmons <joshua.timmons1@gmail.com>
2019-08-28 14:28:33 -0400

    Bump version: 0.4.16 → 0.4.17

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ project = "synbio"
 author = "jjtimmons"
 
 # The full version, including alpha/beta/rc tags
-release = "0.4.16"
+release = "0.4.17"
 
 
 # -- General configuration ---------------------------------------------------
